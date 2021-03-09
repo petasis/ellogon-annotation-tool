4190d342c2823f37a5adfc3fefc487f46d5e937c
Grannita <granna_93@yahoo.gr>
2021-03-09 17:09:01 +0200

    reset settings

--- ellogon_annotation_tool/django_main_app/settings.py
+++ ellogon_annotation_tool/django_main_app/settings.py
@@ -98,15 +98,15 @@ WSGI_APPLICATION = 'django_main_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
+     #'default': {
+         #'ENGINE': 'django.db.backends.sqlite3',
+         #'NAME': BASE_DIR / 'db.sqlite3',
          #'ENGINE': 'django.db.backends.mysql',
          #'OPTIONS': {
          #    'read_default_file': str(BASE_DIR / 'my.cnf'),
          #},
-     }
-    #'default': env.db()
+     #}
+    'default': env.db()
 }
 
 # REST
