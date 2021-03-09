24b4ed3d9e5de7d1097ff8b493088b277d59f6d7
Clarin EL <clarinel@ellogon.iit.demokritos.gr>
2021-03-09 20:40:50 +0200

    Build script

--- build_project.sh
+++ build_project.sh
@@ -3,5 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/ellogon_annotation_tool
 
 git pull
+npm install
 npm run build
 echo yes | python manage.py collectstatic
